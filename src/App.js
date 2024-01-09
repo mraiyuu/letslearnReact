d01fa79a5138412640ed0498e6f2e8e931caeb34
mraiyuu <munenedenis618@gmail.com>
2024-01-09 07:19:10 +0300

    spread oparators

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
    <div className='App'>
-   <p>Hello world!</p>
+   const numberone = [1, 2 , 3];
+   const numbertwo = [4, 5, 6];
+    const numberscombined = [...numberone, ...numbertwo];
+
    </div>
   );
 }
